694e05c3e93edde224290c970458e25e3a4fa94d
ZohaibAliJerov <zuhaib911alijerov@gmail.com>
2024-09-18 19:53:46 +0500

    added a para in projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -19,6 +19,12 @@
     <h2 class="text-[40px] text-center text-white font-causten">
       Our Projects
     </h2>
+    <div class="flex justify-center">
+      <img src="../assets/rectangle2.png" alt="">
+  </div>
+  <div class="w-[90%] text-center text-[#EFEFEF] font-gotham pt-4 leading-[32px]">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
   </div>
   <div>
     {#each projectSets as project}
